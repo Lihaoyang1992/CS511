0ef9eb150004a7d053cd6a5013391e88c383d4ae
Haoyang Li <lee19921009@gmail.com>
2015-10-24 19:19:45 -0400

    add monitor.h

---
+++ monitor.h
@@ -0,0 +1,26 @@
+#ifndef __MONITOR_H__
+#define __MONITOR_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include "cart.h"
+#include "q.h"
+
+pthread_mutex_t	monitor;
+pthread_mutex_t	north_rail;
+pthread_mutex_t	west_rail;
+pthread_mutex_t	south_rail;
+pthread_mutex_t	east_rail;
+
+extern void monitor_init();
+extern void monitor_arrive(struct cart_t *);
+extern void monitor_cross(struct cart_t *);
+extern void monitor_leave(struct cart_t *);
+extern void monitor_shutdown();
+
+extern int get_right_dir(char dir);
+
+#endif /* __MONITOR_H__ */
