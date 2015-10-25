989b5f6430b042fb7bef22865433c26334a4c3be
Haoyang Li <lee19921009@gmail.com>
2015-10-24 20:41:27 -0400

    add break point

--- trafficmgr.c
+++ trafficmgr.c
@@ -130,6 +130,21 @@ main(int argc, char *argv[])
 		}
 	}
 
+	for (i = 0; i < DIR; i++) {
+		if (i == 0)
+			(void)sprintf(stderr, "north queue has %d carts",
+							q_cartIsWaiting(Q_NORTH));
+		else if (i == 1)
+			(void)sprintf(stderr, "west queue has %d carts",
+							q_cartIsWaiting(Q_WEST));
+		else if (i == 2)
+			(void)sprintf(stderr, "south queue has %d carts",
+							q_cartIsWaiting(Q_SOUTH));
+		else if (i == 3)
+			(void)sprintf(stderr, "east queue has %d carts",
+							q_cartIsWaiting(Q_EAST));
+	}
+
 	monitor_shutdown();
 	q_shutdown();
 	return 0;
